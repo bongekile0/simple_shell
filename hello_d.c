b3760f22a8d48c2766beddf3b0a61faf9257154c
bongekile0 <mahlabangane.co.za@gmail.com>
2023-10-17 13:12:41 -0700

    hello_d.c

---
+++ hello_d.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * hello - program the argumant
+ * Retoun(0)
+ *
+ */
+
+int hello(void)
+{
+	print(betty pass);
+
+	retoun (0);
+}
